11caa4a5dd9274091e4a1b3b57d583140ed0d9df
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-08-21 08:36:05 +0900

    update README

--- src/sketch.js
+++ src/sketch.js
@@ -1,4 +1,32 @@
-let d3svg = makeD3Svg();
+/**
+ * Sample draw Rectangle
+ */
+
+function draw (rects) {
+    let d3svg = makeD3Svg();
+    let svg = d3svg.Svg();
+    let forground = svg.selectAll('g.base.forground');
+
+    forground.selectAll('rect.sample')
+        .data(rects, (d) => { return d._id; })
+        .enter()
+        .append('rect')
+        .attr('class', 'sample')
+        .attr('x', (d) => { return d.x;})
+        .attr('y', (d) => { return d.y;})
+        .attr('width', (d) => { return d.w;})
+        .attr('height', (d) => { return d.h;})
+        .attr('rx', (d) => { return d.r;})
+        .attr('ry', (d) => { return d.r;})
+        .attr('fill', (d) => {
+            console.log(d.background.color);
+            return d.background.color;
+        })
+        .attr('stroke', (d) => { return d.stroke.color; })
+        .attr('stroke-width', (d) => { return d.stroke.width; });
+
+}
+
 
 let rects = [
     {
@@ -17,23 +45,5 @@ let rects = [
     }
 ];
 
-let svg = d3svg.Svg();
-let forground = svg.selectAll('g.base.forground');
 
-forground.selectAll('rect.sample')
-    .data(rects, (d) => { return d._id; })
-    .enter()
-    .append('rect')
-    .attr('class', 'sample')
-    .attr('x', (d) => { return d.x;})
-    .attr('y', (d) => { return d.y;})
-    .attr('width', (d) => { return d.w;})
-    .attr('height', (d) => { return d.h;})
-    .attr('rx', (d) => { return d.r;})
-    .attr('ry', (d) => { return d.r;})
-    .attr('fill', (d) => {
-        console.log(d.background.color);
-        return d.background.color;
-    })
-    .attr('stroke', (d) => { return d.stroke.color; })
-    .attr('stroke-width', (d) => { return d.stroke.width; });
+draw(rects);
