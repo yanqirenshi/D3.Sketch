1b1c1652475a2eb0555a1665e5917bc24edf5297
yanqirenshi <yanqirenshi@gmail.com>
2018-12-19 16:54:07 +0900

    add class of Sketcher

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const _DATA = [
+    {
+        x: 0,
+        y: 0,
+        w: 222,
+        h: 333,
+        r: 8,
+        stroke: {
+            color: '#333333',
+            width: 1,
+        },
+        background: {
+            color: '#fff'
+        }
+    }
+];
