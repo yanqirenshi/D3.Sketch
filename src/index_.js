70238990f75f1117b20aa504a2c9ac71424a13cd
yanqirenshi <yanqirenshi@gmail.com>
2019-02-19 13:30:15 +0900

    rname file

---
+++ src/index_.js
@@ -0,0 +1,17 @@
+function dump() {
+    if (arguments.length==0) return;
+    if (arguments.length==1) console.log(arguments[0]);
+
+    console.log(arguments);
+}
+(function draw () {
+    let d3svg = makeD3Svg();
+    let svg = d3svg.Svg();
+    let forground = svg.selectAll('g.base.forground');
+
+    new Sketcher()
+        .data(state)
+        .sizing()
+        .positioning()
+        .draw(forground);
+}());
