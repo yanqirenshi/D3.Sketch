f5a27afe03654c6692ca96eb3ecb34353dc01dfa
yanqirenshi <yanqirenshi@gmail.com>
2019-02-19 13:29:35 +0900

    add func of dump

--- src/template.index.js
+++ src/template.index.js
@@ -1,3 +1,10 @@
+function dump() {
+    if (arguments.length==0) return;
+    if (arguments.length==1) console.log(arguments[0]);
+
+    console.log(arguments);
+}
+
 (function draw () {
     let d3svg = makeD3Svg();
     let svg = d3svg.Svg();
